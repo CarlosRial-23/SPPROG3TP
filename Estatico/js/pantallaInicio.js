20c93675d089d4c0be11bed69b04a99fab8931d2
CarlosRial-23 <rialcarlos2009@hotmail.com>
2025-06-06 20:20:59 -0300

    primer commit

---
+++ Estatico/js/pantallaInicio.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const inicio = document.getElementById('inicio');
+  inicio.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const nombre = document.getElementById('nombre').value.trim();
+    if (nombre) {
+      localStorage.setItem('nombreUsuario', nombre);
+      window.location.href = 'productos.html';
+    }
+  });
+});
