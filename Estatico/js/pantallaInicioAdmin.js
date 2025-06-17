87ac39cc063f79230f492b2749636fba17b8c89e
LucasAmarilla93 <lucas.amarilla.93@gmail.com>
2025-06-17 00:41:13 -0300

    Btn Inicio Admin

---
+++ Estatico/js/pantallaInicioAdmin.js
@@ -0,0 +1,19 @@
+function clickerBtnIngresar() {
+  document.addEventListener("DOMContentLoaded", () => {
+    const inicio = document.getElementById("inicio");
+    inicio.addEventListener("submit", function (e) {
+      e.preventDefault();
+      const nombre = document.getElementById("nombre").value.trim();
+      const password = document.getElementById("password").value;
+      if (nombre && password) {
+        localStorage.setItem("nombreUsuario", nombre);
+        window.location.href = "/admin/productos.html";
+      } else {
+        alert("Por favor completar todos los campos.")
+      }
+    });
+  });
+}
+
+
+clickerBtnIngresar();
